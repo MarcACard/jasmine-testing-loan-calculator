a7e3cca15e72aa78d3058b33e5737a075b2af4bf
Marc Card <marcacard@gmail.com>
2021-04-02 17:43:00 -0700

    update calculateMonthlyPayment & update function functions

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       update();
     });
@@ -26,12 +26,21 @@ function setupIntialValues() {
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const monthlyPayment = calculateMonthlyPayment(getCurrentUIValues());
+  updateMonthly(monthlyPayment);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const { amount, years, rate } = values;
+  const annualRate = rate / 100; // Convert input from percentage to decimal value.
+  const periodicRate = annualRate / 12;
+  let monthlyPayment = (amount * periodicRate) / (1 - (1 + periodicRate) ** (years * 12 * -1));
+
+  monthlyPayment = monthlyPayment.toFixed(2);
+  return monthlyPayment;
 }
 
 // Given a string representing the monthly payment value,
